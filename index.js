50cb5cee6c7319d380ea1da69c58057fb277be25
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-05 21:14:03 -0600

    ✨ added response in json

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html')
 })
 
+app.get('/feedbacks', (req, res) => {
+	res.json({
+		status: 'ok',
+		success: true,
+		data: [
+			{
+				title: 'Error en el servidor',
+			},
+		],
+	})
+})
+
+app.post('/feedbacks', (req, res) => {
+	res.json({
+		status: 'ok',
+		success: true,
+		data: [],
+	})
+})
+
 app.use((req, res) => {
 	res.status(404).sendFile(__dirname + '/public/404.html')
 })
