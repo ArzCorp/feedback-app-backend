2e1c03f9a10737fab6959d38dc97c3e87c6a01cf
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-07 22:32:10 -0600

    💫 added template response

--- src/controllers/feedbacks.controller.js
+++ src/controllers/feedbacks.controller.js
@@ -1,18 +1,23 @@
 import { db } from '../db.js'
+import { templateResponse } from '../utils/templateResponse.js'
 
 export const getFeedbacks = async (req, res) => {
-	const feedbacks = await db.query('SELECT * FROM feedbacks')
-	res.send(feedbacks)
+	const [rows] = await db.query('SELECT * FROM feedbacks')
+	res.send({
+		...templateResponse,
+		data: rows,
+	})
 }
 
 export const createFeedback = async (req, res) => {
 	console.log(req.body)
 	const { title, detail, tagId, ranking, statusId } = req.body
 
-	const feedbacks = await db.query(
+	await db.query(
 		`INSERT INTO feedbacks.feedbacks (title, detail, tagId, ranking, statusId)
-  VALUES (?, ?, ?, ?, ?);`,
+    VALUES (?, ?, ?, ?, ?);`,
 		[title, detail, tagId, ranking, statusId]
 	)
-	res.sendStatus(204)
+
+	res.send({ ...templateResponse, message: 'Feedback created' })
 }
---
+++ src/utils/templateResponse.js
@@ -0,0 +1,7 @@
+export const templateResponse = {
+	status: 'ok',
+	success: true,
+	data: [],
+	message: '',
+	error: null,
+}
