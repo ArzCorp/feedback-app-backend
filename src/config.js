5a6dcdede9425132b332256fda74833bfbb4c4b3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-08 19:42:41 -0600

    💫 added db port

--- src/config.js
+++ src/config.js
@@ -7,3 +7,4 @@ export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'Osva-0807_SQL $'
 export const DB_NAME = process.env.DB_NAME || 'feedbacks'
+export const DB_PORT = process.env.DB_PORT || 3306
